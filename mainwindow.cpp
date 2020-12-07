808a5191ab927cfc2c0c29a32304bd868e24ae56
jian sun <75183234+jessesun-cloud@users.noreply.github.com>
2020-12-06 21:23:47 -0800

    dialog add *.ptx

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -25,20 +25,23 @@ MainWindow::MainWindow()
   setMinimumSize(600, 400);
 
   // create menu
-  QMenuBar *menuBar = new QMenuBar;
+  QMenuBar* menuBar = new QMenuBar;
   setMenuBar(menuBar);
-  QMenu *fileMenu = menuBar->addMenu(tr("&File"));
-  QAction *openFile = new QAction(tr("&Open"), fileMenu);
+  QMenu* fileMenu = menuBar->addMenu(tr("&File"));
+  QAction* openFile = new QAction(tr("&Open"), fileMenu);
   fileMenu->addAction(openFile);
   connect(openFile, &QAction::triggered, this, &MainWindow::_openFileDialog);
-  QAction *closeView = new QAction(tr("&Close"), fileMenu);
+  QAction* closeView = new QAction(tr("&Close"), fileMenu);
   fileMenu->addAction(closeView);
   connect(closeView, &QAction::triggered, this, &MainWindow::_closeView);
 
   // take first command line argument as a path to PLY files
-  if (QApplication::arguments().size() > 1) {
+  if (QApplication::arguments().size() > 1)
+  {
     _openView(QApplication::arguments()[1]);
-  } else {
+  }
+  else
+  {
     // place some hints on a screen
     auto welcomeHint = new QLabel();
     welcomeHint->setTextFormat(Qt::RichText);
@@ -69,22 +72,28 @@ MainWindow::MainWindow()
 
 void MainWindow::_openFileDialog()
 {
-  const QString filePath = QFileDialog::getOpenFileName(this, tr("Open PLY file"), "", tr("PLY Files (*.ply)"));
-  if (!filePath.isEmpty()) {
+  const QString filePath = QFileDialog::getOpenFileName(this, tr("Open PLY file"), "",
+                           tr("Cloud Files (*.ply *.ptx)"));
+  if (!filePath.isEmpty())
+  {
     _openView(filePath);
   }
 }
 
 
-void MainWindow::_openView(const QString& filePath) {
+void MainWindow::_openView(const QString& filePath)
+{
   _closeView();
 
-  try {
+  try
+  {
     // create new new
     setCentralWidget(new Viewer(filePath));
     // add source path into title
     setWindowTitle(QString("%1 - %2").arg(filePath).arg(TITLE));
-  } catch (const std::exception& e) {
+  }
+  catch (const std::exception& e)
+  {
     QMessageBox::warning(this, tr("Cannot open view"), e.what());
   }
 }
@@ -92,7 +101,8 @@ void MainWindow::_openView(const QString& filePath) {
 
 void MainWindow::_closeView()
 {
-  if (centralWidget()) {
+  if (centralWidget())
+  {
     // destroy view
     centralWidget()->close();
     takeCentralWidget()->deleteLater();
